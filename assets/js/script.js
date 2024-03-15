4124969f957df6656dcb674338392adf44689113
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-03-15 10:14:07 +0000

    add code to subtract question

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,6 +70,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"]
     } else if (operator === "x"){
         return [operand1 * operand2, "multiply"]
+    } else if (operator === "-"){
+        return [operand1 - operand2, "subtract"]
     }
     else {
         alert(`Unimplemented operator ${operator}`)
@@ -99,11 +101,15 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion(){}
-
-function displayMultiplyQuestion(operand1, operand2){
+function displaySubtractQuestion(operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
+}
+
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "x";
 
 }
