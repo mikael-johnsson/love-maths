674374113aa1ab89055f62ae7a02e1f44867a753
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-03-15 08:59:55 +0000

    add code to calculate two random numbers for the question

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+//Wait for DOM
+//Get button elements and add event listener
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons)
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+
+})
+
+/**
+ * The main game "loop"
+ */
+function runGame(){
+    // Creates two random umbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25 + 1)
+    let num2 = Math.floor(Math.random()*25 + 1)
+}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractQuestion(){}
+
+function displayMultiplyQuestion(){}
+
